1e66949bc3cf93b5e7dd2cd488d1a29d57f6758f
Van Quang <vanquang.3799@gmail.com>
2020-06-15 23:15:24 +0700

    Fix loi tim kiem

--- resources/views/admin/all_game.blade.php
+++ resources/views/admin/all_game.blade.php
@@ -7,15 +7,7 @@
       Các Game Đang Bán
     </div>
     <div class="row w3-res-tb">
-      <div class="col-sm-5 m-b-xs">
-        <select class="input-sm form-control w-sm inline v-middle">
-          <option value="0">Bulk action</option>
-          <option value="1">Delete selected</option>
-          <option value="2">Bulk edit</option>
-          <option value="3">Export</option>
-        </select>
-        <button class="btn btn-sm btn-default">Apply</button>                
-      </div>
+      
       <div class="col-sm-4">
       </div>
     </div>
